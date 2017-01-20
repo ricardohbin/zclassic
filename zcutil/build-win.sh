2c132e636b18384f6b4229709c6aef232a1801f3
joshuayabut <yabut.joshua@gmail.com>
2017-01-19 22:08:31 -0500

    fixup! Working build

--- zcutil/build-win.sh
+++ zcutil/build-win.sh
@@ -12,5 +12,5 @@ cd "$(dirname "$(readlink -f "$0")")/.."
 cd depends/ && make HOST=$HOST V=1 NO_QT=1 && cd ../
 ./autogen.sh
 CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site CXXFLAGS+=" -fopenmp" ./configure --prefix="${PREFIX}" --with-gui=no --host="${HOST}" --enable-static --disable-shared
-sed -i 's/-lboost_system-mt /-lboost_system-mt-s /' Makefile
+sed -i 's/-lboost_system-mt /-lboost_system-mt-s /' configure
 CC="${CC}" CXX="${CXX}" make V=1
